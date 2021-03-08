08f4ea5aeb93ed6d1ded971b51ae023c44145fea
Natan Humieniuk <nhumieniuk@bsd220.org>
2021-03-08 10:41:14 -0600

    Added user location

--- Za Hunter/ContentView.swift
+++ Za Hunter/ContentView.swift
@@ -6,12 +6,26 @@
 //
 
 import SwiftUI
-
+import MapKit
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Map(
+            coordinateRegion: $region,
+            interactionModes: .all,
+            showsUserLocation: true,
+            userTrackingMode: $userTrackingMode
+        )
     }
+@StateObject var locationManager = LocationManager()
+@State private var userTrackingMode: MapUserTrackingMode = .follow
+@State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(
+        latitude: 42.15704,
+        longitude: -88.14812),
+        span: MKCoordinateSpan(
+        latitudeDelta: 0.05,
+        longitudeDelta: 0.05)
+        )
 }
 
 struct ContentView_Previews: PreviewProvider {
